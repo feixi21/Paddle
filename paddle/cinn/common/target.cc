ef9a011281bd043a5690d18adeb7203471da8353
feixi21 <1802550529@qq.com>
2024-10-20 20:37:33 +0800

    fix dcu compiler

--- paddle/cinn/common/target.cc
+++ paddle/cinn/common/target.cc
@@ -242,6 +242,7 @@ std::string Target::arch_str() const {
 }
 
 std::string Target::device_name_str() const {
+  #ifdef CINN_WITH_CUDA
   int device_idx = 0;
   cudaError_t result = cudaGetDevice(&device_idx);
   if (result != cudaSuccess) {
@@ -265,6 +266,7 @@ std::string Target::device_name_str() const {
   std::string device_name = properties.name;
   device_name = std::regex_replace(device_name, std::regex(" "), "_");
   return std::regex_replace(device_name, std::regex("-"), "_");
+  #endif
 }
 
 std::ostream &operator<<(std::ostream &os, const Target &target) {
