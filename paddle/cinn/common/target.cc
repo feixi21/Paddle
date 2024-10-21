b9fcedb1596d3760b5ac23a50700172e38b0011d
feixi21 <1802550529@qq.com>
2024-10-21 15:33:20 +0800

    fix_paddle

--- paddle/cinn/common/target.cc
+++ paddle/cinn/common/target.cc
@@ -363,6 +363,7 @@ const Target &DefaultHygonDcuSyclTarget() {
 const Target &DefaultDeviceTarget() {
 #ifdef CINN_WITH_CUDA
   return DefaultNVGPUTarget();
+
 #elif defined(CINN_WITH_HIP)
   return DefaultHygonDcuHipTarget();
 #elif defined(CINN_WITH_SYCL)
@@ -404,6 +405,7 @@ int GetMaxBlocks() {
 const Target &DefaultTarget() {
 #ifdef CINN_WITH_CUDA
   return DefaultNVGPUTarget();
+
 #elif defined(CINN_WITH_HIP)
   return DefaultHygonDcuHipTarget();
 #elif defined(CINN_WITH_SYCL)
